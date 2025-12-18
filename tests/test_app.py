3a1f1bdd0affac12546302e61ee8e29b32d6a50f
Stepan Zyryanov <spznsk@inbox.ru>
2025-12-19 00:23:18 +0700

    РГЗ

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,12 @@
+import os, sys
+
+# добавляем корень проекта в sys.path, чтобы работало и локально, и в Actions
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from app import app
+
 
 def test_weather_ok():
     client = app.test_client()
     r = client.get("/weather/?city=Amsterdam")
     assert r.status_code == 200
-    body = r.get_json()
-    assert body["data"]["city"] == "Amsterdam"
